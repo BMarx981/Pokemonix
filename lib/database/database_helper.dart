22324a51f4b4b81289b5e3da0efea3a8f1501b9e
BMarx981 <b.marx981@gmail.com>
2020-06-26 15:11:36 -0400

    Add database to the project.

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,12 @@
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:flutter/foundation.dart';
+
+class DatabaseHelper {
+  final String dbName = 'pokemondb';
+  final int version = 1;
+
+  final String table = 'pokeTable';
+
+  final String 
+}
